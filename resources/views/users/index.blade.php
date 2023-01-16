2814249c23bcea53be45796a37b58f5eb0f3c29c
cmwakichi <mmwambela@gmail.com>
2023-01-16 18:58:13 +0300

    pushed modal to the right

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -43,4 +43,20 @@
             </div>
         </div>
     </div>
+
+    {{-- modal --}}
+    <div class="modal right fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Sidebar</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @include('layouts.includes.sideBar')
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
