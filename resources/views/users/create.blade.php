ca7cac2bad5291ed70aa3b00300b3d64ebdfb142
cmwakichi <mmwambela@gmail.com>
2023-01-16 23:02:27 +0300

    designed the user create form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -13,16 +13,32 @@
                             @csrf
                             <div class="form-group">
                                 <label for="">Name</label>
-                                <input type="text" name="name" class="form-control">
+                                <input type="text" name="name" class="form-control mb-3">
                             </div>
                             <div class="form-group">
                                 <label for="">Email</label>
-                                <input type="text" name="email" class="form-control">
+                                <input type="text" name="email" class="form-control mb-3">
                             </div>
                             <div class="form-group">
                                 <label for="">Phone</label>
-                                <input type="text" name="phone" class="form-control">
+                                <input type="text" name="phone" class="form-control mb-3">
                             </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" name="password" class="form-control mb-3">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Confirm Password</label>
+                                <input type="password" name="confirm_pwd" class="form-control mb-3">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Role</label>
+                                <select type="text" name="is_admin" class="form-control mb-3">
+                                    <option value="1">Admin</option>
+                                    <option value="2">Cashier</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary ">Save User</button>
                         </form>
                     </div>
                 </div>
