b661c9537f11513b0ac762d0da6676e47e227993
cmwakichi <mmwambela@gmail.com>
2023-01-16 17:26:26 +0300

    designing the navbar with the icons

--- resources/views/layouts/includes/navBar.blade.php
+++ resources/views/layouts/includes/navBar.blade.php
@@ -2,13 +2,28 @@
     <i class="fa fa-list"></i>
 </a>
 <a href="#" class="btn btn-outline rounded-pill">
-    <i class="fa fa-list"></i>
+    <i class="fa fa-user">Users</i>
 </a>
 <a href="#" class="btn btn-outline rounded-pill">
-    <i class="fa fa-list"></i>
+    <i class="fa fa-truck">Products</i>
 </a>
 <a href="#" class="btn btn-outline rounded-pill">
-    <i class="fa fa-list"></i>
+    <i class="fa fa-laptop">Cashier</i>
+</a>
+<a href="#" class="btn btn-outline rounded-pill">
+    <i class="fa fa-file">Reports</i>
+</a>
+<a href="#" class="btn btn-outline rounded-pill">
+    <i class="fa fa-money-bill">Transactions</i>
+</a>
+<a href="#" class="btn btn-outline rounded-pill">
+    <i class="fa fa-chart">Suppliers</i>
+</a>
+<a href="#" class="btn btn-outline rounded-pill">
+    <i class="fa fa-user-group">Customers</i>
+</a>
+<a href="#" class="btn btn-outline rounded-pill">
+    <i class="fa fa-chart">Incoming</i>
 </a>
 <style>
     .btn-outline {
