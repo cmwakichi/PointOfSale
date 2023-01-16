398f8661211c2abb5f0874512748f601debfe06d
cmwakichi <mmwambela@gmail.com>
2023-01-16 18:58:50 +0300

    added a blank modal body

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -54,7 +54,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    @include('layouts.includes.sideBar')
+
                 </div>
             </div>
         </div>
