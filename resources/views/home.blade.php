269f7f1061d86c454611e69ec9c1d787000b7899
cmwakichi <mmwambela@gmail.com>
2023-01-16 17:49:30 +0300

    added the welcomer banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-9">
+            <div class="col-md-9 mt-3">
                 <div class="card">
-                    <div class="card-header">
-                        {{ __('Dashboard') }}
-                    </div>
+                    <h4 class="card-header">
+                        <marquee behaviour="">Welcome to POS Management System.</marquee>
+                    </h4>
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">{{ session('status') }}</div>
@@ -15,7 +15,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 mt-3">
                 <div class="card">
                     <div class="card-header">
                         {{ __('Dashboard') }}
