ce976b2aa4997441f01c6e3d2ca2b10cb27cef09
cmwakichi <mmwambela@gmail.com>
2023-01-29 11:13:38 +0300

    added products drop down inside the orders index

---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 style="float: left">Order Products</h4>
+                            <a href="{{ route('products.create') }}" style="float: right" class="btn btn-dark"><i
+                                    class="fa fa-plus"></i>
+                                Add new order
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered table-left">
+                                <thead>
+                                    <tr>
+                                        <th>#Id</th>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Brand</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <select name="product_id" id="product_id" class="form-control">
+                                                @foreach ($products as $product)
+                                                    <option value="{{ $product->id }}">{{ $product->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Search product</h4>
+                        </div>
+                        <div class="card-body">
+                            ..........
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
