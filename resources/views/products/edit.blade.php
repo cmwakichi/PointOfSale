7af179fb1d14a0a4be315888222c5bceb6d8ea73
cmwakichi <mmwambela@gmail.com>
2023-01-29 09:35:19 +0300

    designed the product edit form

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-md-7">
+            <div class="row">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Edit Product Details Form</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('products.update') }}" method="post">
+                            @csrf
+                            @method('put')
+                            <div class="form-group">
+                                <label for="">Product Name</label>
+                                <input type="text" name="name" value="{{ $product->name }}" class="form-control mb-3">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Description</label>
+                                <textarea name="description" cols="30" rows="2" class="form-control mb-3">{{ $product->description }}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Brand</label>
+                                <input type="text" name="brand" value="{{ $product->brand }}"
+                                    class="form-control mb-3">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Price</label>
+                                <input type="number" name="price" value="{{ $product->price }}"
+                                    class="form-control mb-3">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Quantity</label>
+                                <input type="number" name="qty" value="{{ $product->qty }}" class="form-control mb-3">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Alert Stock</label>
+                                <input type="number" name="alert_stock" value="{{ $product->alert_stock }}"
+                                    class="form-control mb-3">
+                            </div>
+                            <button type="submit" class="btn btn-primary ">Update Product</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
